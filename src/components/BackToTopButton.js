bb786565e89f7ffadac5a07872681aa00bd4924b
fededraetta <federicodraetta@yahoo.it>
2023-01-22 20:00:26 +0100

    nicer back to top

--- src/components/BackToTopButton.js
+++ src/components/BackToTopButton.js
@@ -1,6 +1,8 @@
 // Create the button
 let button = document.createElement("button");
-button.innerHTML = "Back to Top";
+
+// Use Font Awesome to create an icon
+button.innerHTML = '<i class="fa fa-arrow-up"></i>';
 
 // Style the button
 button.style.display = "none";
@@ -8,11 +10,11 @@ button.style.position = "fixed";
 button.style.bottom = "20px";
 button.style.right = "30px";
 button.style.zIndex = "99";
-button.style.fontSize = "18px";
+button.style.fontSize = "30px";
 button.style.border = "none";
 button.style.outline = "none";
-button.style.backgroundColor = "rgb(0, 0, 0)";
-button.style.color = "rgb(255, 255, 255)";
+button.style.backgroundColor = "rgba(0, 0, 0,0)";
+button.style.color = "rgb(80, 142, 155)";
 button.style.cursor = "pointer";
 button.style.padding = "15px";
 button.style.borderRadius = "4px";

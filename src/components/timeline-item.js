157dac2f8de95e7968921d06c87ec1aef68f9131
fededraetta <federicodraetta@yahoo.it>
2023-01-24 18:03:39 +0100

    final (?) version of timeline

--- src/components/timeline-item.js
+++ src/components/timeline-item.js
@@ -1,18 +1,32 @@
-const timelineItems = document.querySelectorAll(".timeline-item");
+(function () {
+  "use strict";
 
-function updateTimelineShadow() {
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    timelineItems.forEach(item => {
-      item.style.setProperty("--shadow-color", "rgba(255, 255, 255, 0.1)");
-    });
-  } else {
-    timelineItems.forEach(item => {
-      item.style.setProperty("--shadow-color", "rgba(0, 0, 0, 0.1)");
-    });
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
   }
-}
 
-updateTimelineShadow();
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
 
-window.matchMedia("(prefers-color-scheme: dark)").addListener(updateTimelineShadow);
-
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();

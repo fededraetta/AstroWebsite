dff2229ffc7e40396613b5659c063cc16bc813d2
fededraetta <federicodraetta@yahoo.it>
2023-01-25 17:06:31 +0100

    sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte()],
+  integrations: [mdx(), svelte(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'nord'

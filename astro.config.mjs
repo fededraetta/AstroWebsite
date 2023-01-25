1161770cd2e3ebdc3ad0cf069552510d38bb9f73
fededraetta <federicodraetta@yahoo.it>
2023-01-25 17:15:16 +0100

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
+  site: 'https://www.federicodraetta.com',
   integrations: [mdx(), svelte(), sitemap()],
   markdown: {
     shikiConfig: {
